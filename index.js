f1ae9d3f32c6e9a3fbf4b46da2afe74cbc197c40
rucha1997 <ruchu.kulkarni@gmail.com>
2022-02-13 04:26:57 +0530

    Added db changes in index.js

--- index.js
+++ index.js
@@ -5,11 +5,18 @@ const port = '8080';
 const bodyParser = require('body-parser');
 const indexRouter = require('./routes/index');
 const logger = require('./config/logger');
+const config = require('./config/db');
+const mongoose = require('mongoose');
 
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 app.use('/', indexRouter);
+mongoose.connect(config.dbUrl, {useNewUrlParser: true});
+const conn = mongoose.connection;
+conn.on('connected', function() {
+    logger.info('Database is connected successfully.');
+});
 app.listen(port, () => {
     logger.info(`Server is running on port ${port}`);
 })
